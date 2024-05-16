dad81e47f87cb814bc05b4ece93eb2f10bc4d56e
Palak <palak@gmail.com>
2024-05-16 23:40:15 +0530

    initial commit

---
+++ src/main/java/jenkinswarfile/jenkinswarfile/JenkinswarfileApplication.java
@@ -0,0 +1,13 @@
+package jenkinswarfile.jenkinswarfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinswarfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinswarfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jenkinswarfile/jenkinswarfile/JenkinswarfileApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkinswarfile.jenkinswarfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinswarfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
